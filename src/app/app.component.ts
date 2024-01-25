f04535020288d08b72cf45c87e0e56b1669faf36
Somesh K Bhaskar <someshkumarbhaskar@gmail.com>
2024-01-25 23:46:16 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import * as THREE from 'three';
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const width = window.innerWidth, height = window.innerHeight;
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    camera.position.z = 1;
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const material = new THREE.MeshNormalMaterial();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    renderer.setAnimationLoop(animation);
+    document.body.appendChild(renderer.domElement);
+
+    // animation
+
+    function animation(time: number) {
+
+      mesh.rotation.x = time / 2000;
+      mesh.rotation.y = time / 1000;
+
+      renderer.render(scene, camera);
+
+    }
+  }
   title = 'VRWorld';
 }
