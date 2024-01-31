fab6f72694d89344d9f5ff95ef23034bcf220fb7
Somesh K Bhaskar <someshkumarbhaskar@gmail.com>
2024-01-31 23:18:34 +0530

    added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,36 @@ import 'aframe';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [{ provide: Document, useValue: Document }],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent implements AfterViewInit {
 
+  @ViewChild("test") myModelEntity: any;
+
+  constructor(private el: ElementRef) { }
+
   ngAfterViewInit(): void {
+
+  }
+
+  handleEntityClick() {
+    console.log("op")
+    const myModelEntity = this.el.nativeElement.querySelector('#myModel');
+    // or
+    // const myModelEntity = this.myModelRef.nativeElement;
+
+    if (myModelEntity) {
+      // Access animations using A-Frame API
+      const animations = myModelEntity.getObject3D('gltf').animations;
+
+      // Log animation information to the console
+      animations.forEach((animation: any, index: any) => {
+        console.log(`Animation ${index}:`, animation);
+      });
+
+      // Play the first animation (adjust the index if needed)
+      myModelEntity.setAttribute('animation-mixer', { clip: animations[0].name, loop: 'repeat' });
+    }
   }
 
 }
